57cddf6e13a83e47372a6a60eac3e35bf3826cb0
mnkkhan <mnkkhan77@gmail.com>
2023-03-06 23:36:13 +0530

    day 2 Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+// Components import
+import Navbar from "./components/Navbar/Navbar";
+import NavbarResponsive from "./components/NavbarResponsive/NavbarResponsive";
+
+const App = () => {
+  const [hamActive, setHamActive] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar hamActive={hamActive} setHamActive={setHamActive} />
+      <NavbarResponsive hamActive={hamActive} />
+      
     </div>
   );
-}
+};
 
 export default App;
